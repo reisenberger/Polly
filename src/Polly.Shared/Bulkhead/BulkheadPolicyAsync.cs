e35563e6c233305483706499feeeefb873515c8b
Bruno Lauze <brunolauze@msn.com>
2016-09-15 15:30:58 -0400

    Adding Bulkhead Policy

---
+++ src/Polly.Shared/Bulkhead/BulkheadPolicyAsync.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Polly.Bulkhead
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class BulkheadPolicy
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="asyncExceptionPolicy"></param>
+        internal BulkheadPolicy(Func<Func<CancellationToken, Task>, Context, CancellationToken, bool, Task> asyncExceptionPolicy)
+           : base(asyncExceptionPolicy, Enumerable.Empty<ExceptionPredicate>())
+        {
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class BulkheadPolicy<TResult>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="asyncExecutionPolicy"></param>
+        internal BulkheadPolicy(
+            Func<Func<CancellationToken, Task<TResult>>, Context, CancellationToken, bool, Task<TResult>> asyncExecutionPolicy
+            ) : base(asyncExecutionPolicy, Enumerable.Empty<ExceptionPredicate>(), Enumerable.Empty<ResultPredicate<TResult>>())
+        {
+        }
+    }
+}
---
+++ src/Polly.Shared/Bulkhead/SemaphoreRejectedException.cs
@@ -0,0 +1,57 @@
+﻿
+namespace Polly.Bulkhead
+{
+    using System;
+    using System.Runtime.Serialization;
+    using System.Runtime.InteropServices;
+
+    /// <summary>
+    /// Class SemaphoreRejectedException.
+    /// </summary>
+#if !PORTABLE
+    [Serializable()]
+#endif
+    [ComVisibleAttribute(false)]
+    public class SemaphoreRejectedException :
+#if PORTABLE
+        Exception
+#else        
+        SystemException
+#endif
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SemaphoreRejectedException" /> class.
+        /// </summary>
+        public SemaphoreRejectedException() : this("The semaphore is full and the call was rejected.")
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SemaphoreRejectedException"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        public SemaphoreRejectedException(String message) : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SemaphoreRejectedException"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="innerException">The inner exception.</param>
+        public SemaphoreRejectedException(String message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+#if !PORTABLE
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SemaphoreRejectedException"/> class.
+        /// </summary>
+        /// <param name="info">The information.</param>
+        /// <param name="context">The context.</param>
+        protected SemaphoreRejectedException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+#endif
+    }
+}
