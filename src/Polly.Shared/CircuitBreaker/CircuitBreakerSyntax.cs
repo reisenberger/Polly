2ce717270f3d07ec26a3caf13415add01a7715c6
reisenberger <software@reisenberger.net>
2017-08-19 00:17:04 +0100

    Add example policy overload from config
    Add example configuration overload configuring a CircuitBreaker policy
    from an IConsecutiveCountCircuitBreakerConfiguration

--- src/Polly.Shared/CircuitBreaker/CircuitBreakerSyntax.cs
+++ src/Polly.Shared/CircuitBreaker/CircuitBreakerSyntax.cs
@@ -168,28 +168,49 @@ namespace Polly
         /// <exception cref="ArgumentNullException">onHalfOpen</exception>
         public static CircuitBreakerPolicy CircuitBreaker(this PolicyBuilder policyBuilder, int exceptionsAllowedBeforeBreaking, TimeSpan durationOfBreak, Action<Exception, TimeSpan, Context> onBreak, Action<Context> onReset, Action onHalfOpen)
         {
+            IConsecutiveCountCircuitBreakerConfiguration configuration = new ConsecutiveCountCircuitBreakerConfiguration(exceptionsAllowedBeforeBreaking, durationOfBreak);
+
+            return policyBuilder.CircuitBreaker(configuration, onBreak, onReset, onHalfOpen);
+        }
+
+        /// <summary>
+        /// <para> Builds a <see cref="Policy"/> that will function like a Circuit Breaker.</para>
+        /// <para>The circuit will break according to the configuration provided in the <see cref="IConsecutiveCountCircuitBreakerConfiguration"/>
+        /// </para>
+        /// </summary>
+        /// <param name="policyBuilder">The policy builder.</param>
+        /// <param name="configuration">The configuration parameters for a <see cref="CircuitBreakerPolicy"/>.</param>
+        /// <param name="onBreak">The action to call when the circuit transitions to an <see cref="CircuitState.Open"/> state.</param>
+        /// <param name="onReset">The action to call when the circuit resets to a <see cref="CircuitState.Closed"/> state.</param>
+        /// <param name="onHalfOpen">The action to call when the circuit transitions to <see cref="CircuitState.HalfOpen"/> state, ready to try action executions again. </param>
+        /// <returns>The policy instance.</returns>
+        /// <remarks>(see "Release It!" by Michael T. Nygard fi)</remarks>
+        /// <exception cref="System.ArgumentOutOfRangeException">exceptionsAllowedBeforeBreaking;Value must be greater than zero.</exception>
+        /// <exception cref="ArgumentNullException">onBreak</exception>
+        /// <exception cref="ArgumentNullException">onReset</exception>
+        /// <exception cref="ArgumentNullException">onHalfOpen</exception>
+        public static CircuitBreakerPolicy CircuitBreaker(this PolicyBuilder policyBuilder, IConsecutiveCountCircuitBreakerConfiguration configuration, Action<Exception, TimeSpan, Context> onBreak, Action<Context> onReset, Action onHalfOpen)
+        {
             if (onBreak == null) throw new ArgumentNullException("onBreak");
             if (onReset == null) throw new ArgumentNullException("onReset");
             if (onHalfOpen == null) throw new ArgumentNullException("onHalfOpen");
 
-            IConsecutiveCountCircuitBreakerConfiguration configuration = new ConsecutiveCountCircuitBreakerConfiguration(exceptionsAllowedBeforeBreaking, durationOfBreak);
-
             var breakerController = new ConsecutiveCountCircuitController<EmptyStruct>(
                 configuration,
                 (outcome, timespan, context) => onBreak(outcome.Exception, timespan, context),
-                onReset, 
+                onReset,
                 onHalfOpen);
             return new CircuitBreakerPolicy(
                 (action, context, cancellationToken) => CircuitBreakerEngine.Implementation(
                     (ctx, ct) => { action(ctx, ct); return EmptyStruct.Instance; },
                     context,
                     cancellationToken,
-                    policyBuilder.ExceptionPredicates, 
-                    PredicateHelper<EmptyStruct>.EmptyResultPredicates, 
-                    breakerController), 
-                policyBuilder.ExceptionPredicates, 
+                    policyBuilder.ExceptionPredicates,
+                    PredicateHelper<EmptyStruct>.EmptyResultPredicates,
+                    breakerController),
+                policyBuilder.ExceptionPredicates,
                 breakerController
-                );
+            );
         }
     }
 }
