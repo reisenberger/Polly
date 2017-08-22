fa4e8dfb54d79aff480e831a57445978b9205aa4
reisenberger <software@reisenberger.net>
2017-08-22 21:51:46 +0100

    Demonstration dynamic reconfiguration validator
    Demonstrates a decorator approach to validating configuration values
    provided by dynamic reconfiguration.  Where users provided their own
    IXYZConfiguration, the policy configuration overload could, internally,
    wrap it with a validator such as this.

---
+++ src/Polly.Shared/CircuitBreaker/ConsecutiveCountCircuitBreakerConfigurationValidator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Polly.CircuitBreaker
+{
+    internal class ConsecutiveCountCircuitBreakerConfigurationValidator : IConsecutiveCountCircuitBreakerConfiguration
+    {
+        private IConsecutiveCountCircuitBreakerConfiguration _wrappedConfigurationProvider;
+
+        public ConsecutiveCountCircuitBreakerConfigurationValidator(IConsecutiveCountCircuitBreakerConfiguration configurationProvider)
+        {
+            _wrappedConfigurationProvider = configurationProvider;
+        }
+
+        public int FailuresAllowedBeforeBreaking
+        {
+            get
+            {
+                int wrappedValue = _wrappedConfigurationProvider.FailuresAllowedBeforeBreaking;
+                if (wrappedValue <= 0) throw new InvalidOperationException("IConsecutiveCountCircuitBreakerConfiguration.FailuresAllowedBeforeBreaking must provide values greater than zero.");
+                return wrappedValue;
+            }
+        }
+
+        public TimeSpan DurationOfBreak
+        {
+            get
+            {
+                TimeSpan wrappedValue = _wrappedConfigurationProvider.DurationOfBreak;
+                if (wrappedValue < TimeSpan.Zero) throw new InvalidOperationException("IConsecutiveCountCircuitBreakerConfiguration.DurationOfBreak must provide values greater than or equal to zero.");
+                return wrappedValue;
+            }
+        }
+    }
+}
