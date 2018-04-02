06d2f3cf951adfd96546e9bcdcc6fd47b201bbb2
reisenberger <software@reisenberger.net>
2018-04-02 14:24:48 +0100

    Add abstracted Stopwatch
    Add abstracted Stopwatch: for timing execution durations; and also
    manipulable, for tests.

---
+++ src/Polly.Shared/Utilities/SystemStopwatch.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Polly.Utilities
+{
+    /// <summary>
+    /// Elapsed time related delegates used to support testability of the code.
+    /// </summary>
+    public static class SystemStopwatch
+    {
+        /// <summary>
+        /// Initializes the <see cref="SystemStopwatch"/> class.
+        /// </summary>
+        static SystemStopwatch()
+        {
+            Stopwatch.Start();
+            Initialise();
+        }
+
+        private static readonly System.Diagnostics.Stopwatch Stopwatch = new System.Diagnostics.Stopwatch();
+
+        /// <summary>
+        /// Allows the setting of a custom StopwatchTimestamp value, for testing.
+        /// By default this will use the <see cref="System.Diagnostics.Stopwatch.GetTimestamp()"/> method.
+        /// </summary>
+        public static Func<long> StopwatchTimestamp;
+
+        /// <summary>
+        /// Allows the setting of a custom StopwatchFrequency value, for testing.
+        /// By default this will use <see cref="m:Stopwatch.Frequency"/>
+        /// </summary>
+        public static long StopwatchFrequency;
+
+        /// <summary>
+        /// Gets the elapsed time since an earlier StopwatchTimestamp.
+        /// </summary>
+        /// <param name="startStopwatchTimestamp">The StopwatchTimestamp </param>
+        public static TimeSpan ElapsedSince(long startStopwatchTimestamp)
+        {
+            return new TimeSpan((long)((StopwatchTimestamp() - startStopwatchTimestamp) * (TimeSpan.TicksPerSecond / (double)StopwatchFrequency)));
+        }
+
+        /// <summary>
+        /// Sets the implementations. 
+        /// </summary>
+        public static void Initialise()
+        {
+            StopwatchTimestamp = System.Diagnostics.Stopwatch.GetTimestamp;
+            StopwatchFrequency = System.Diagnostics.Stopwatch.Frequency;
+        }
+
+        /// <summary>
+        /// Resets the custom implementations to their defaults. 
+        /// Should be called during test teardowns.
+        /// </summary>
+        public static void Reset()
+        {
+            Initialise();
+        }
+    }
+}
---
+++ src/Polly.SharedSpecs/Utilities/SystemStopwatchSpecs.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using FluentAssertions;
+using Polly.Utilities;
+using Xunit;
+
+namespace Polly.Specs.Utilities
+{
+    [Collection(Polly.Specs.Helpers.Constants.AbstractedTimeDependentTestCollection)]
+    public class SystemStopwatchSpecs : IDisposable
+    {
+        [Fact]
+        public void Should_give_realistic_elapsed_value_with_default_implementation()
+        {
+            long start = SystemStopwatch.StopwatchTimestamp();
+
+            TimeSpan expectedDuration = TimeSpan.FromSeconds(1);
+            SystemClock.Sleep(expectedDuration, CancellationToken.None);
+
+            SystemStopwatch.ElapsedSince(start).Should().BeCloseTo(expectedDuration, precision: 250); // Somewhat broad precision, to allow for variability in running, in CI environments.
+        }
+
+        [Fact]
+        public void Should_give_custom_elapsed_value_with_overridden_implementation()
+        {
+            SystemStopwatch.StopwatchFrequency = TimeSpan.TicksPerSecond / TimeSpan.TicksPerMillisecond; // Override with a custom implementation scaled in Milliseconds.
+
+            long start = 0;
+            SystemStopwatch.StopwatchTimestamp = () => start + SystemStopwatch.StopwatchFrequency; // Manually advance the exact units for one second.
+
+            SystemStopwatch.ElapsedSince(0).Should().Be(TimeSpan.FromSeconds(1));
+        }
+
+
+
+        public void Dispose()
+        {
+            SystemStopwatch.Reset();
+        }
+
+    }
+}
